9007f6c697ae281590115a78b6b1709da9f3d68b
David Lee <dhlee@usc.edu>
2022-12-11 10:52:19 -0800

    update

--- nvim/.config/nvim/lua/core/alpha.lua
+++ nvim/.config/nvim/lua/core/alpha.lua
@@ -1,24 +1,48 @@
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
+
 local M = {}
 
 local function configure_additional_autocmds()
-  local group = "_dashboard_settings"
-  vim.api.nvim_create_augroup(group, {})
-  vim.api.nvim_create_autocmd("FileType", {
-    group = group,
-    pattern = "alpha",
-    command = "set showtabline=0 | autocmd BufLeave <buffer> set showtabline=" .. vim.opt.showtabline._value,
-  })
-  -- https://github.com/goolord/alpha-nvim/issues/42
-  vim.api.nvim_create_autocmd("FileType", {
-    group = group,
-    pattern = "alpha",
-    command = "set laststatus=0 | autocmd BufUnload <buffer> set laststatus=" .. vim.opt.laststatus._value,
-  })
+	local group = "_dashboard_settings"
+	vim.api.nvim_create_augroup(group, {})
+	vim.api.nvim_create_autocmd("FileType", {
+		group = group,
+		pattern = "alpha",
+		command = "set showtabline=0 | autocmd BufLeave <buffer> set showtabline=" .. vim.opt.showtabline._value,
+	})
+	-- https://github.com/goolord/alpha-nvim/issues/42
+	vim.api.nvim_create_autocmd("FileType", {
+		group = group,
+		pattern = "alpha",
+		command = "set laststatus=0 | autocmd BufUnload <buffer> set laststatus=" .. vim.opt.laststatus._value,
+	})
+end
+
+local function footer()
+	local total_plugins = #vim.tbl_keys(packer_plugins)
+	local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
+	local version = vim.version()
+	local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+
+	return datetime .. "   " .. total_plugins .. " plugins" .. nvim_version_info
 end
 
 function M.setup()
-  require("alpha").setup(require("alpha.themes.dashboard").config)
-  configure_additional_autocmds()
+	dashboard.section.buttons.val = {
+		dashboard.button("<leader>r", "  Recent", ":Telescope oldfiles<CR>"),
+		dashboard.button("<leader>n", "  Neu", ":ene <BAR> startinsert <CR>"),
+		dashboard.button("<leader>f", "  Find Local", ":Telescope find_files cwd=.<CR>"),
+		dashboard.button("<leader>g", "  Find Global", ":Telescope find_files cwd=~<CR>"),
+		dashboard.button("<leader>p", "  Find Development", ":Telescope find_files search_dirs=['~/dev']<CR>"),
+		dashboard.button("<leader>p", "  Configurations", ":Telescope find_files cwd=~/.config/nvim/<CR>"),
+	}
+
+	dashboard.section.footer.val = footer()
+	dashboard.section.footer.opts.hl = "Constant"
+
+	require("alpha").setup(dashboard.opts)
+	configure_additional_autocmds()
 end
 
 return M
