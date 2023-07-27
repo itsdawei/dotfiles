d7ad5ef83e523bdc96bd4acb56061651cc81e2aa
itsdawei <dhlee@usc.edu>
2023-07-27 02:28:59 -0700

    Gitsigns

--- nvim/.config/nvim/lua/core/gitsigns.lua
+++ nvim/.config/nvim/lua/core/gitsigns.lua
@@ -34,17 +34,16 @@ M.setup = function()
 				linehl = "GitSignsChangeLn",
 			},
 		},
-		numhl = false,
-		linehl = false,
-		keymaps = {
-			-- Default keymap options
-			noremap = true,
-			buffer = true,
-		},
-		signcolumn = true,
-		word_diff = false,
-		attach_to_untracked = true,
-		current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+    numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+    linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+    word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    watch_gitdir = {
+      interval=1000,
+      follow_files = true
+    },
+    attach_to_untracked = true,
+    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 		current_line_blame_opts = {
 			virt_text = true,
 			virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
@@ -54,22 +53,18 @@ M.setup = function()
 		current_line_blame_formatter_opts = {
 			relative_time = false,
 		},
+		sign_priority = 6,
+		update_debounce = 200,
+		status_formatter = nil, -- Use default
 		max_file_length = 40000,
 		preview_config = {
 			-- Options passed to nvim_open_win
-			border = "rounded",
+			border = "single",
 			style = "minimal",
 			relative = "cursor",
 			row = 0,
 			col = 1,
 		},
-		watch_gitdir = {
-			interval = 1000,
-			follow_files = true,
-		},
-		sign_priority = 6,
-		update_debounce = 200,
-		status_formatter = nil, -- Use default
 		yadm = { enable = false },
 	})
 end
