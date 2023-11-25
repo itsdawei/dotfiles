b8704af1c31d44def067047b13738947b1bd0d89
itsdawei <dhlee@usc.edu>
2023-11-25 14:02:23 -0800

    nvim config update

---
+++ nvim/.config/nvim/lua/core/indent_blankline.lua
@@ -0,0 +1,59 @@
+local M = {}
+
+M.setup = function()
+	local status_ok, ibl = pcall(require, "ibl")
+	if not status_ok then
+		return
+	end
+	ibl.setup({
+		indent = {
+			char = { "", "┊", "┆", "¦", "|", "¦", "┆", "┊", "" },
+		},
+		exclude = {
+			buftypes = { "terminal", "nofile" },
+			filetypes = {
+				"alpha",
+				"log",
+				"gitcommit",
+				"dapui_scopes",
+				"dapui_stacks",
+				"dapui_watches",
+				"dapui_breakpoints",
+				"dapui_hover",
+				"LuaTree",
+				"dbui",
+				"UltestSummary",
+				"UltestOutput",
+				"vimwiki",
+				"markdown",
+				"json",
+				"txt",
+				"vista",
+				"NvimTree",
+				"git",
+				"TelescopePrompt",
+				"undotree",
+				"flutterToolsOutline",
+				"org",
+				"orgagenda",
+				"help",
+				"startify",
+				"dashboard",
+				"lazy",
+				"neogitstatus",
+				"Outline",
+				"Trouble",
+				"lspinfo",
+				"", -- for all buffers without a file type
+			},
+		},
+		scope = {
+			enabled = true,
+			show_start = false,
+		},
+	})
+	local hooks = require("ibl.hooks")
+	hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+end
+
+return M
